15ab6843a43bddfa463f7be57381be1329741ffe
gabrielkinyua <gabzkinyua@gmail.com>
2023-03-31 22:32:53 +0000

    Add basic C source file

--- 0_functions.c
+++ 0_functions.c
@@ -1,18 +1,27 @@
+#include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
-
+#include <string.h>
 void print_char(va_list args)
 {
 	char c = va_arg(args, int);
 	write(1, &c, 1);
 }
 
-void print_string(va_list args)
+void print_string(va_list args, int *count)
 {
 	int i = 0;
 	char *s = va_arg(args, char *);
-	for (; str[i] != '\0'; i++)
+	if (s == NULL)
 	{
-		write(1, &str[i], i);
+		write(1, "(null)", 6);
+		(*count) += 6;
+		return;
+	}
+	for (; s[i] != '\0'; i++)
+	{
+		write(1, &s[i], 1);
+		(*count)++;
 	}
 }
 
