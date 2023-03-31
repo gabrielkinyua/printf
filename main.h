d0ecaea04f5e9a85900169c94866d3863c348d42
gabrielkinyua <gabzkinyua@gmail.com>
2023-03-31 22:32:06 +0000

    Add C header file

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
+#include <unistd.h>
+#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 void print_char(va_list args);
-void print_string(va_list args);
+void print_string(va_list args, int *count);
 void print_percent(void);
 
 #endif /* MAIN_H */
